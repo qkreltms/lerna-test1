fcc39b21bb51c9c1b124ab9ca653a680fc881f3d
qkreltms <junghooncentralpark@gmail.com>
2021-02-10 20:54:00 +0900

    initial commit

---
+++ packages/button/src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Button = () => (
+  <button type="button">test</button>
+    )
+
+export default Button
---
+++ packages/button2/src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Button = () => (
+  <button type="button">test2</button>
+    )
+
+export default Button
