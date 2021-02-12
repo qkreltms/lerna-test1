abfb97d0fc1d79649ab4ccd89257a8a355ce7788
qkreltms <junghooncentralpark@gmail.com>
2021-02-13 00:21:27 +0900

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,8 +6,22 @@ module.exports = {
     "../**/stories/**/*.stories.@(js|jsx|ts|tsx|scss)",
   ],
   addons: [
-    "@storybook/preset-scss",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
   ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader?url=false', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
+  },
 };
